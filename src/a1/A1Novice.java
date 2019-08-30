61aac01aa92347606c7172ffd4a299db3128332a
golcott <48141153+golcott@users.noreply.github.com>
2019-08-30 17:52:57 -0400

    Fixed Novice

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,30 +10,22 @@ public class A1Novice {
 
 		// Your code follows here.
 		int datapnts = scan.nextInt();
-		// String[] customer = new String[datapnts];
-		// System.out.println(datapnts);
 		for (int i=0; i<datapnts; i++) {
 			String first = scan.next();
-			// System.out.println("after first name");
 			char firstInitial = first.charAt(0);
-			// System.out.println("after first initial");
 			String last = scan.next();
-			// System.out.println("after last name");
 			double total = 0;
 			int numOfItems = scan.nextInt();
-			// System.out.println(numOfItems);
 			for (int j=0; j<numOfItems; j++) {
-				// System.out.println("after inner for loop");
 				int quant = scan.nextInt();
-				// System.out.println("before");
 				String food = scan.next();
-				// System.out.println("after");
 				double price = scan.nextDouble();
 				for (int k=0; k<quant; k++) {
 					total = total + price;
 				}
 			}
-		System.out.println(firstInitial + ". " + last + ": " + total);
+		String fixTotal = String.format("%.2f", total);
+		System.out.println(firstInitial + ". " + last + ": " + fixTotal);
 		}		
 	scan.close();
 	}
