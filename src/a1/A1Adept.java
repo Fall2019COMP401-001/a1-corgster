3c9daca1c5c6f20e306540d6f3dc3b7995e9df6c
golcott <48141153+golcott@users.noreply.github.com>
2019-08-30 16:42:07 -0400

    Finished Novice

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -9,6 +9,50 @@ public class A1Adept {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
-		
+		int datapnts = scan.nextInt();
+		double [] item = new double [datapnts];
+		for (int i=0; i<item.length; i++) {
+			String itemName = scan.next();
+			double price = scan.nextDouble();
+		}
+		int customers = scan.nextInt();
+		double [] requests = new double [customers];
+		for (int j=0; j<requests.length; j++) {
+			String first = scan.next();
+			String last = scan.next();
+			int numOfItems = scan.nextInt();
+			double total = 0;
+			for (int k=0; k<numOfItems; k++) {
+				int quant = scan.nextInt();
+				String itemID = scan.next();
+			}
+			System.out.println(first + " " + last + " (" + total + ")");
+		}
+		/* int datapnts = scan.nextInt();
+		String[] itemName = new String [datapnts];
+		double[] itemPrice = new double [datapnts];
+		for (int i=0; i<itemName.length; i++) {
+			itemName[i] = scan.next();
+			itemPrice[i] = scan.nextDouble();
+		}
+		double total = 0;
+		int customers = scan.nextInt();
+		double [] requests = new double [customers];
+		for (int j=0; j<requests.length; j++) {
+			String first = scan.next();
+			String last = scan.next();
+			int numOfItems = scan.nextInt();
+			for (int k=0; k<numOfItems; k++) {
+				int quant = scan.nextInt();
+				String itemID = scan.next();
+				for (int l=0; l<itemName.length; l++) {
+					if (itemID==itemName[l]) {
+					total = (total + itemPrice[l]) * quant;
+					}	
+				}
+			}
+		System.out.println(first + " " + last + " (" + total + ")");
+		}
+		*/
 	}
 }
--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,32 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
-		
+		int datapnts = scan.nextInt();
+		// String[] customer = new String[datapnts];
+		// System.out.println(datapnts);
+		for (int i=0; i<datapnts; i++) {
+			String first = scan.next();
+			// System.out.println("after first name");
+			char firstInitial = first.charAt(0);
+			// System.out.println("after first initial");
+			String last = scan.next();
+			// System.out.println("after last name");
+			double total = 0;
+			int numOfItems = scan.nextInt();
+			// System.out.println(numOfItems);
+			for (int j=0; j<numOfItems; j++) {
+				// System.out.println("after inner for loop");
+				int quant = scan.nextInt();
+				// System.out.println("before");
+				String food = scan.next();
+				// System.out.println("after");
+				double price = scan.nextDouble();
+				for (int k=0; k<quant; k++) {
+					total = total + price;
+				}
+			}
+		System.out.println(firstInitial + ". " + last + ": " + total);
+		}		
+	scan.close();
 	}
 }
